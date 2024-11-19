b587c61d24d5c9cbad7600df9b11d2dc9e21368a
Alex <o.moliaka@gmail.com>
2024-11-19 16:04:40 +0100

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@ int main(){
 
 	std::cout << "Hello world!" << std::endl;
 	std::cout << "second line" << std::endl;
-	std::cout << "third line" << std:: endl;
+	std::cout << "third line" << std::endl;
 	return 0;
 
 }
